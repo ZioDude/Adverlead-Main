ff08a2dd236fe73cb89bb46186033082c13663fa
ZioDude <akyriakouu@gmail.com>
2025-05-22 01:53:30 +0300

    fix4

--- src/app/api/ad-drafts/[id]/route.ts
+++ src/app/api/ad-drafts/[id]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
 export async function DELETE(
-  request: Request,
+  _request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
